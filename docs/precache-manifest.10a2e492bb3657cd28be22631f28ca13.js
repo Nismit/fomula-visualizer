cfc805ca4665499df991d269351d658d0582275a
git <nismit.dev@gmail.com>
2019-05-15 23:29:06 -0700

    v1.0.0

---
+++ docs/precache-manifest.10a2e492bb3657cd28be22631f28ca13.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bdcd3ef03d973f429f79e411183831f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7cb436bf97eccecc0b9c",
+    "url": "/static/css/main.4c0301eb.chunk.css"
+  },
+  {
+    "revision": "358b01e6c42e1994edd1",
+    "url": "/static/js/2.e6e7b995.chunk.js"
+  },
+  {
+    "revision": "7cb436bf97eccecc0b9c",
+    "url": "/static/js/main.8c3b0454.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
