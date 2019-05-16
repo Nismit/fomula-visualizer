7c5ef5a706a1293e916c4dadc9438bf9c0382313
git <nismit.dev@gmail.com>
2019-05-15 23:25:14 -0700

    Update link for noise functions

--- src/App.js
+++ src/App.js
@@ -152,9 +152,9 @@ class App extends React.Component {
             vec2 uv - By default, it has been multiplied by 2.5<br />
             <br />
             Functions:<br />
-            <a href="#">float snoise(vec2 v)</a><br />
-            <a href="#">float cnoise(vec2 P)</a><br />
-            <a href="#">float pnoise(vec2 P, vec2 rep)</a><br />
+            <a href="https://github.com/Nismit/fomula-visualizer/blob/master/src/fragment.js#L33-L79" target="_blank" rel="noopener noreferrer">float snoise(vec2 v)</a><br />
+            <a href="https://github.com/Nismit/fomula-visualizer/blob/master/src/fragment.js#L112-L149" target="_blank" rel="noopener noreferrer">float cnoise(vec2 P)</a><br />
+            <a href="https://github.com/Nismit/fomula-visualizer/blob/master/src/fragment.js#L152-L189" target="_blank" rel="noopener noreferrer">float pnoise(vec2 P, vec2 rep)</a><br />
           </p>
         </div>
 
